351c883e06c33f892922bc6a17c02e7b0edbef49
Lukas Stavaris <lukas.stavaris@mif.stud.vu.lt>
2024-11-08 10:05:31 +0200

    Cleaned upand readied disasm code

--- disasm.asm
+++ disasm.asm
@@ -3,12 +3,10 @@
 
 .data
     buff db 200h dup(?)
-    file_name db 13 dup(?)
-    file_handle dw ?
-    out_file_name db "out.txt",0
+    in_file_name db 13 dup(?)
+    in_file_handle dw ?
+    out_file_name db 13 dup(?)
     out_file_handle dw ?
-    letter_count dw 26 dup(0)
-    total_count dw 0
     error_file db "No file with such name: $"
     error_arg db "No arguments were provided", 0Ah, 24h
     endl db 0Dh,0Ah, 24h
@@ -18,12 +16,6 @@
 start:
     MOV dx, @data
     MOV ds, dx
-
-    MOV ah, 3Ch
-    MOV cx, 0
-    MOV dx, offset out_file_name
-    INT 21h
-    MOV out_file_handle, ax
 
     XOR cx, cx
     MOV cl, es:[80h]
@@ -39,70 +31,86 @@ start:
 
         ;End of arg
         CMP al, " "
-        JE open_in_file
+        JE open_files
         CMP cx, 1
-        JE open_in_file
+        JE open_files
 
         ;Not end of arg
-        mov ds:[file_name + bx], al
+        MOV [in_file_name + bx], al
+        MOV [out_file_name + bx], al
+
         INC bx
     continue:
         INC si
     LOOP args
 
-    CALL exit
+    JMP exit
 
 err_arguments:
     MOV ah, 09h
-    MOV dx, offset error_arg
+    LEA dx, error_arg
     INT 21h
 
-    CALL exit
+    JMP exit
 
-err_opening:
-    MOV ah, 09h
-    MOV dx, offset error_file
-    INT 21h
-
-    MOV ah, 40h
-    mov cx, bx
-    mov bx, 01h
-    MOV dx, offset file_name
-    INT 21h
-
-    MOV ah, 09h
-    MOV dx, offset endl
-    INT 21h
-
-    MOV ax, 4c00h
-    INT 21h
-
-    CALL exit
-
-open_in_file:
+open_files:
     PUSH cx
 
-    mov ds:[file_name + bx], 0
+    MOV [out_file_name + bx - 4], "."
+    MOV [out_file_name + bx - 3], "a"
+    MOV [out_file_name + bx - 2], "s"
+    MOV [out_file_name + bx - 1], "m"
 
-    ; Opening file
-    mov ah, 3Dh
-    mov al, 00
-    mov dx, offset file_name
-    int 21h
+    ; Opening in file
+    MOV ax, 3d00h
+    LEA dx, in_file_name
+    INT 21h
     JC err_opening
-    MOV [file_handle], ax
+    MOV [in_file_handle], ax
 
-    call process_file
+    ; Opening out file
+    MOV ah, 3Ch
+    MOV cx, 0
+    LEA dx, out_file_name
+    INT 21h
+    MOV out_file_handle, ax
+
+    CALL process_file
 
     ; Closing file
-    mov ah, 3Eh
-    mov dx, [file_handle]
-    int 21h
+    MOV ah, 3Eh
+    MOV dx, out_file_handle
+    INT 21h
+
+    ; Closing file
+    MOV ah, 3Eh
+    MOV dx, in_file_handle
+    INT 21h
 
     POP cx
     MOV bx, 0
 
     JMP continue
+
+err_opening:
+    MOV ah, 09h
+    LEA dx, error_file
+    INT 21h
+
+    MOV ah, 40h
+    MOV cx, bx
+    MOV bx, 01h
+    LEA dx, in_file_name
+    INT 21h
+
+    MOV ah, 09h
+    LEA dx, endl
+    INT 21h
+
+    MOV ax, 4c00h
+    INT 21h
+
+    JMP exit
 
 process_file:
     RET
